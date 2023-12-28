e8bee9f0beb571eb5b02b808af26018fcc567fef
amangh30 <108615374+amangh30@users.noreply.github.com>
2023-12-28 20:27:11 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,37 +1,43 @@
-import {Server} from "socket.io"
+import { Server } from 'socket.io';
 
-const  io = new Server(9000,{
-    cors:{
-        origin: true
-    }
+const io = new Server(9000, {
+    cors: {
+        origin: 'http://localhost:3000',
+    }, 
 })
+
 
 let users = [];
 
-const addUser = (userData,socketId)=>{
-    !users.some(user => user.sub == userData.sub) && users.push({...userData,socketId});
+const addUser = (userData, socketId) => {
+    !users.some(user => user.sub === userData.sub) && users.push({ ...userData, socketId });
 }
 
 const removeUser = (socketId) => {
     users = users.filter(user => user.socketId !== socketId);
 }
 
-const getUser = (userId)=>{
-    return users.find(user => user.sub ===userId);
+const getUser = (userId) => {
+    return users.find(user => user.sub === userId);
 }
 
-io.on('connection',(socket)=>{
-    console.log("user connected")
-    socket.on("addUsers",userData =>{
-        addUser(userData,socket.id)
-        io.emit("getUsers",users);
-    })
-    
-    socket.on('sendMessage',data =>{
-        const user = getUser(data.receiverId)
-        io.to(user?.socketId).emit('getMessage',data)
+io.on('connection',  (socket) => {
+    console.log('user connected')
 
-        socket.on('disconnect', () => {
+    //connect
+    socket.on("addUsers", userData => {
+        addUser(userData, socket.id);
+        io.emit("getUsers", users);
+    })
+
+    //send message
+    socket.on('sendMessage', (data) => {
+        const user = getUser(data.receiverId);
+        io.to(user.socketId).emit('getMessage', data)
+    })
+
+    //disconnect
+    socket.on('disconnect', () => {
         console.log('user disconnected');
         removeUser(socket.id);
         io.emit('getUsers', users);
